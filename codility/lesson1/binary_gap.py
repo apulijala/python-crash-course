ca87244895461538f46f86990acae308f1f4ba58
apulijala <arvind_puljala@hotmail.com>
2020-05-29 23:52:09 +0100

    Test the string

--- codility/lesson1/binary_gap.py
+++ codility/lesson1/binary_gap.py
@@ -1,29 +1,31 @@
 def solution(N):
-    flag = True
     bin_str = get_binary_string_from_num(N)
-    print("binary string", bin_str)
+    # print("binary string", bin_str)
     start = 0
     maxlen = 0
-    while flag:
+    while True:
         try:
             start = bin_str.index("1", start)
-            tosearch = start  + 1
-            nextone = bin_str.index("1",tosearch)
+            # because of test cases i can do this
+            # tosearch = start  + 1
+            nextone = bin_str.index("1",start  + 1)
             length = (nextone - start) - 1
-            print(f"start={start}",f"nextone={nextone}", f"length={length}" )
-            # start = nextone +1
-            start = nextone 
             if length > maxlen:
                 maxlen = length
+            # print(f"start={start}",f"nextone={nextone}", f"length={length}" )
+            # start = nextone +1. this one took the nextone to next start off. 
+            start = nextone 
         except ValueError as ve:
-            flag = False
+           break
     return maxlen
         
 
     
 
 def get_binary_string_from_num(N):
-    q = N
+    return bin(N)
+    """
+    q = N # This is imporant as it reduces.
     (q,r) = divmod(q,2)
     binstr = ""
     binstr = str(r) + binstr
@@ -31,3 +33,4 @@ def get_binary_string_from_num(N):
         (q,r) = divmod(q,2)
         binstr = str(r) + binstr
     return binstr
+    """
