d6a6f881a44cd33dd16b806038392e9c9ee4ee49
Arvind K. Pulijala <arvind_pulijala@hotmail.com>
2020-06-02 22:43:58 +0100

    effecient test passing cars

--- codility/lesson5/passing_cars.py
+++ codility/lesson5/passing_cars.py
@@ -1,4 +1,6 @@
 def passing_cars(A):
+
+    A = [0, 1, 0, 1, 1]
     num_passing = 0
     n = len(A)
     for i in range(0,n):
@@ -10,5 +12,23 @@ def passing_cars(A):
                         return -1
 
     return num_passing
+
+
+def passing_cars_effecient(A):
+    count_zeros = 0
+    num_passing = 0
+    i = 0
+    n = len(A)
+    while i < n:
+        if A[i] == 0:
+            count_zeros += 1
+        else: # Assume 1 as it is only possiblity.
+            num_passing += count_zeros
+            if num_passing > 1_000_000_000:
+                return -1 
+            
+        i = i + 1
+    return num_passing
+
 
         
--- codility/lesson5/test_passing_cars.py
+++ codility/lesson5/test_passing_cars.py
@@ -2,9 +2,15 @@ import unittest
 from passing_cars import *
 
 class PassingCars(unittest.TestCase):
+    @unittest.skip
     def test_passing_cars(self):
         A = [0, 1, 0, 1, 1]
         result = passing_cars(A)
+        self.assertEqual(result, 5)
+    
+    def test_passing_cars_effecient(self):
+        A = [0, 1, 0, 1, 1]
+        result = passing_cars_effecient(A)
         self.assertEqual(result, 5)
 
 
